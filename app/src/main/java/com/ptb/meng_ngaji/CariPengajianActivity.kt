f8847124faeee20c2b694f58ab829b574269bdb9
kintanputia <kintanputia090101@gmail.com>
2021-10-07 02:47:47 +0700

    progres ui 1

---
+++ app/src/main/java/com/ptb/meng_ngaji/CariPengajianActivity.kt
@@ -0,0 +1,11 @@
+package com.ptb.meng_ngaji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CariPengajianActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cari_pengajian)
+    }
+}
